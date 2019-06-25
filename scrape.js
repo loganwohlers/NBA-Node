018c7d41847f2f4f8df90f9090041bd9146de79a
Logan Wohlers <lawohlers@gmail.com>
2019-06-25 13:52:42 -0700

    fully scraped season averages and saved to hash

--- scrape.js
+++ scrape.js
@@ -1,24 +1,24 @@
 const cheerio = require('cheerio')
 // const tableParser = require('cheerio-tableparser')
 const request = require('request')
-// const url = 'https://www.basketball-reference.com/leagues/NBA_2019_per_game.html'
-
 
 const data = []
 
 const url = 'https://www.basketball-reference.com/leagues/NBA_2019_per_game.html'
+
 request(url, (err, res, html) => {
     if (!err && res.statusCode === 200) {
         const $ = cheerio.load(html)
         const tableBody = $('#per_game_stats').children('tbody')
-
         tableBody.find('tr').each((index, ele) => {
-            let row = []
+            let row = {}
             $(ele).find('td').each((index, ele) => {
-                row.push($(ele).text())
+                let statName = $(ele).data().stat
+                let statVal = $(ele).text()
+                row[statName] = statVal
+
             })
             data.push(row)
-
         })
         console.log(data)
 
