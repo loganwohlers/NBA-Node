1353db4ae45d7608ceda541cf2f3b317eaf5e4b2
Logan Wohlers <lawohlers@gmail.com>
2019-06-25 13:29:28 -0700

    reading table

--- scrape.js
+++ scrape.js
@@ -1,4 +1,5 @@
 const cheerio = require('cheerio')
+// const tableParser = require('cheerio-tableparser')
 const request = require('request')
 // const url = 'https://www.basketball-reference.com/leagues/NBA_2019_per_game.html'
 
@@ -6,7 +7,17 @@ const url = 'https://www.basketball-reference.com/leagues/NBA_2019_per_game.html
 request(url, (err, res, html) => {
     if (!err && res.statusCode === 200) {
         const $ = cheerio.load(html)
-        const table = $('#div_per_game_stats')
-        console.log(table.html())
+        const tableBody = $('#per_game_stats').children('tbody')
+
+        // .children('tbody')
+        //fulltable
+        // console.log(tableBody.find('tr').find('td').first().text())
+        // tableBody.find('tr').each((index) => {
+        //     console.log($(this).html())
+        // })
+        tableBody.find('tr').each((index, ele) => {
+            console.log($(ele).find('td').first().text())
+        })
+
     }
 })
