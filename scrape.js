4a96f72f68b8f3c9acd0a39a4219120dfbf8d56b
Logan Wohlers <lawohlers@gmail.com>
2019-06-25 13:40:51 -0700

    scraping every row

--- scrape.js
+++ scrape.js
@@ -3,21 +3,25 @@ const cheerio = require('cheerio')
 const request = require('request')
 // const url = 'https://www.basketball-reference.com/leagues/NBA_2019_per_game.html'
 
+
+const data = []
+
 const url = 'https://www.basketball-reference.com/leagues/NBA_2019_per_game.html'
 request(url, (err, res, html) => {
     if (!err && res.statusCode === 200) {
         const $ = cheerio.load(html)
         const tableBody = $('#per_game_stats').children('tbody')
 
-        // .children('tbody')
-        //fulltable
-        // console.log(tableBody.find('tr').find('td').first().text())
-        // tableBody.find('tr').each((index) => {
-        //     console.log($(this).html())
-        // })
         tableBody.find('tr').each((index, ele) => {
-            console.log($(ele).find('td').first().text())
+            let row = []
+            $(ele).find('td').each((index, ele) => {
+                row.push($(ele).text())
+            })
+            data.push(row)
+
         })
+        console.log(data)
 
     }
 })
+
