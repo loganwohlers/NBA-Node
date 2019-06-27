27bca12d395be7e3216c992ae8559cd8df5b394a
Logan Wohlers <lawohlers@gmail.com>
2019-06-26 19:12:12 -0700

    removed player season model alltogether

--- src/models/player.js
+++ src/models/player.js
@@ -1,5 +1,120 @@
 const mongoose = require('mongoose')
-const { PlayerSeason, playerSeasonSchema } = require('./player-season')
+
+const playerSeasonSchema = new mongoose.Schema({
+    team_id: {
+        type: String,
+        default: 0
+    },
+    age: {
+        type: String,
+        default: 18
+    },
+    g: {
+        type: String,
+        default: 0
+    },
+    gs: {
+        type: String,
+        default: 0
+    },
+    pos: {
+        type: String,
+        default: 0
+    },
+    fg2_per_g: {
+        type: String,
+        default: 0
+    },
+    fg2a_per_g: {
+        type: String,
+        default: 0
+    },
+    fg2_pct: {
+        type: String,
+        default: 0
+    },
+    trb_per_g: {
+        type: String,
+        default: 0
+    },
+    mp_per_g: {
+        type: String,
+        default: 0
+    },
+    fg_per_g: {
+        type: String,
+        default: 0
+    },
+    fga_per_g: {
+        type: String,
+        default: 0
+    },
+    fg_pct: {
+        type: String,
+        default: 0
+    },
+    fg3_per_g: {
+        type: String,
+        default: 0
+    },
+    fg3a_per_g: {
+        type: String,
+        default: 0
+    },
+    fg3_pct: {
+        type: String,
+        default: 0
+    },
+    efg_pct: {
+        type: String,
+        default: 0
+    },
+    ft_per_g: {
+        type: String,
+        default: 0
+    },
+    fta_per_g: {
+        type: String,
+        default: 0
+    },
+    ft_pct: {
+        type: String,
+        default: 0
+    },
+    orb_per_g: {
+        type: String,
+        default: 0
+    },
+    drb_per_g: {
+        type: String,
+        default: 0
+    },
+    ast_per_g: {
+        type: String,
+        default: 0
+    },
+    stl_per_g: {
+        type: String,
+        default: 0
+    },
+    blk_per_g: {
+        type: String,
+        default: 0
+    },
+    tov_per_g: {
+        type: String,
+        default: 0
+    },
+    pf_per_g: {
+        type: String,
+        default: 0
+    },
+    pts_per_g: {
+        type: String,
+        default: 0
+    },
+
+})
 
 const playerSchema = new mongoose.Schema({
     name: {
@@ -9,6 +124,7 @@ const playerSchema = new mongoose.Schema({
     },
     seasons: [playerSeasonSchema]
 })
+
 
 const Player = mongoose.model('Player', playerSchema)
 
@@ -23,7 +139,7 @@ module.exports = { Player, playerSchema }
 
     //child
 //     teamSeasons[]
-
+// ____________________
 // TeamSeasons: 
 //  {win, 
 // loss, 
