d086699e0ddc012b425e913c3e710c35cce1e42e
Logan Wohlers <lawohlers@gmail.com>
2019-06-27 12:40:08 -0700

    refactor on seeding

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,7 +6,7 @@ const scrapePlayerSeasons = require('../../scrape/player-season')
 //ip address instead of localhost
 const URL = 'mongodb://127.0.0.1:27017/nba-api'
 
-seedTeams = async (connectionURL) => {
+seedDB = async (connectionURL) => {
     try {
         await mongoose.connect(connectionURL, {
             useNewUrlParser: true,
@@ -16,10 +16,16 @@ seedTeams = async (connectionURL) => {
     } catch (e) {
         return console.log(e)
     }
-
     const db = mongoose.connection.db
-    db.dropCollection('teams')
+    db.dropCollection('players')
+    // db.dropCollection('teams')
 
+    // await seedTeams()
+    await seedPlayers(2019)
+    console.log('db seeded')
+}
+
+seedTeams = async () => {
     try {
         await Team.insertMany(teams)
         console.log('teams seeded')
@@ -28,31 +34,14 @@ seedTeams = async (connectionURL) => {
     }
 }
 
-//rewrite everything w/o then?
-dbSetUp = async (connectionURL) => {
-    try {
-        await mongoose.connect(connectionURL, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        })
-    } catch (e) {
-        return console.log(e)
-    }
-    console.log('connected to MongoDB')
-
-    //could split this off as it's own async fn-- that uses the connect as a paramter?  not sure if that's allowed
-    const db = mongoose.connection.db
-    //clearing out players collection for a re-seed.  not the right way to do this probably
-    db.dropCollection('players')
-
+seedPlayers = async (yr) => {
     //the .then here should be it's own fn-- maybe create arr of players and then do a mass insertMany?
     //need to stop dupe players-- want them to just update their seasons
     let scrapedData
     let season
     try {
-        scrapedData = await scrapePlayerSeasons(2019)
-        season = await Season.findOne({ year: 2019 })
+        scrapedData = await scrapePlayerSeasons(yr)
+        season = await Season.findOne({ year: yr })
     } catch (e) {
         return console.log(e)
     }
@@ -72,9 +61,7 @@ dbSetUp = async (connectionURL) => {
                 name
             })
         }
-
         let obj = { ...data[i] }
-
         //TOT=Total Over Teams-- average stats of one player across multiple teams in a season
         //might add later but not now
         if (obj.team_id !== 'TOT') {
@@ -89,7 +76,6 @@ dbSetUp = async (connectionURL) => {
             }
         }
     }
-    console.log('seeded players/seasons')
 }
 
 seedRandom = async (connectionURL) => {
@@ -117,4 +103,4 @@ seedRandom = async (connectionURL) => {
 // seedRandom(URL)
 // seedTeams(URL)
 
-dbSetUp(URL)
+seedDB(URL)
--- src/models/game.js
+++ src/models/game.js
@@ -1,11 +1,53 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-    date: {
+    gameCode: {
         type: String,
         trim: true,
         required: true
-    }
+    },
+    date: {
+        type: String,
+        trim: true,
+    },
+    game_start_time: {
+        type: String,
+        trim: true,
+    },
+    away_team: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Team',
+    },
+    visitor_pts: {
+        type: String,
+        trim: true,
+    },
+    home_team: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Team',
+    },
+    home_pts: {
+        type: String,
+        trim: true,
+    },
+    overtimes: {
+        type: String,
+        trim: true,
+    },
+    attendance: {
+        type: String,
+        trim: true,
+    },
+    game_remarks: {
+        type: String,
+        trim: true,
+    },
+    season: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Season',
+    },
+
+
 })
 //teams,score, time, location,  boxscore [] of lines
 
