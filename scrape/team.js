28a139f5b6aba21b870c11363b2b5542109cffce
Logan Wohlers <lawohlers@gmail.com>
2019-07-02 18:33:28 -0700

    starting to scrape team data per season but will clean up season as a whole fist

--- scrape/team.js
+++ scrape/team.js
@@ -1,8 +1,28 @@
-//div id = meta --for win/loss and other info
-//ex URL https://www.basketball-reference.com/teams/ATL/2019.html
-//** code/yr ATL/2019.html
-// const teams = require('../assets/teams')
-// const { Team } = require('../src/models/team')
+const cheerio = require('cheerio')
+const request = require('request-promise')
+const teams = require('../assets/teams')
+const { Team } = require('../src/models/team')
 
-// let tt = new Team(teams[0])
-// console.log(tt)
+scrapeTeamSeasons = async (yr) => {
+    const result = []
+    const url = 'https://www.basketball-reference.com/teams/' + code + '/' + yr + '.html'
+    const testUrl = 'https://www.basketball-reference.com/teams/MIL/2019.html'
+    let data
+    try {
+        data = await request(testUrl)
+    } catch (e) {
+        return console.log(e)
+    }
+    const $ = cheerio.load(data)
+    const tableBody = $('#team_and_opponent').children('tbody')
+    tableBody.find('tr').each((index, ele) => {
+        let row = {}
+        $(ele).find('td').each((index, ele) => {
+            let statName = $(ele).data().stat
+            let statVal = $(ele).text()
+            row[statName] = statVal
+        })
+        result.push(row)
+    })
+    return result
+}
