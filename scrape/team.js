a3f11f65ebe6438acefa4f1f7e46f20a4dcce1fa
Logan Wohlers <lawohlers@gmail.com>
2019-07-07 16:29:43 -0700

    need to map home/opponent data for each team

--- scrape/team.js
+++ scrape/team.js
@@ -1,5 +1,17 @@
 const puppeteer = require('puppeteer')
 
+
+mapHomeAwayData = (seasonData) => {
+    let homeSorted = seasonData.team.sort((a, b) => a.team_name - b.team_name)
+    let oppSorted = seasonData.opp.sort((a, b) => a.team_name - b.team_name)
+
+    console.log(homeSorted)
+    console.log(oppSorted)
+
+    let final = { team: {}, opponent: {} }
+    return final
+}
+
 scrapeTeamData = async (year) => {
     console.log('starting puppeteer')
     const browser = await puppeteer.launch();
@@ -7,18 +19,18 @@ scrapeTeamData = async (year) => {
     let page = await browser.newPage();
     await page.goto(URL);
     const data = await page.evaluate(() => {
-        let results = []
         let team = []
         let opp = []
 
         let teamRows = document.querySelectorAll("#team-stats-per_game tbody tr");
         let oppRows = document.querySelectorAll("#opponent-stats-per_game tbody tr");
-        for (let i = 0; i < 3; i++) {
+        for (let i = 0; i < 5; i++) {
             let teamData = {}
             let oppData = {}
 
             let teamVals = teamRows[i].querySelectorAll('td')
             let oppVals = oppRows[i].querySelectorAll('td')
+
             if (teamVals && oppVals) {
                 for (let i = 0; i < teamVals.length; i++) {
                     let teamStatName = teamVals[i].dataset.stat
@@ -32,27 +44,19 @@ scrapeTeamData = async (year) => {
                     oppData[oppStatName] = oppStatVal
                 }
                 team.push(teamData)
-                opp.push(teamData)
+                opp.push(oppData)
             }
         }
-        mapHomeAwayData(team, opp)
-
+        let results = { team, opp }
         return results
     })
     console.log('closing!')
+
+    mapHomeAwayData(data)
     await browser.close()
-    console.log(data)
     return data
 }
 
-mapHomeAwayData = (team, opp) => {
-    let homeSorted = team.sort((a, b) => a.team_name - b.team_name)
-    let oppSorted = opp.sort((a, b) => a.team_name - b.team_name)
-
-    console.log(homeSorted)
-
-    let final = { team: {}, opponent: {} }
-}
 
 
 scrapeTeamData(2019)
