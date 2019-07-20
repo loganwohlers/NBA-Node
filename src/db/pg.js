5b207daec80cf7a3776b85ec27a7315c77590c7c
Logan Wohlers <lawohlers@gmail.com>
2019-07-20 15:51:29 -0700

    connected to pg

--- src/db/pg.js
+++ src/db/pg.js
@@ -1,10 +1,17 @@
 const { Client } = require('pg')
-const client = new Client()
+
+const connectionString = 'postgresql://postgres:password@localhost:5432/dvdrental'
+
+const client = new Client({
+    connectionString
+})
 
 pgConnect = async () => {
     await client.connect()
     const res = await client.query('SELECT $1::text as message', ['Hello world!'])
     console.log(res.rows[0].message) // Hello world!
+    // const res2 = await client.query('SELECT * FROM film LIMIT 1;')
+    // console.log(res2)
     await client.end()
 }
 
