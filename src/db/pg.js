3c273625d7ba779c112593992ddcf0caa22ed834
Logan Wohlers <lawohlers@gmail.com>
2019-07-20 16:05:58 -0700

    actually able to query nba db

--- src/db/pg.js
+++ src/db/pg.js
@@ -1,17 +1,35 @@
 const { Client } = require('pg')
 
-const connectionString = 'postgresql://postgres:password@localhost:5432/dvdrental'
+const connectionString = 'postgresql://postgres:password@localhost:5432/nbanode'
 
 const client = new Client({
     connectionString
 })
 
+// const text = 'INSERT INTO users(name, email) VALUES($1, $2) RETURNING *'
+// const values = ['brianc', 'brian.m.carlson@gmail.com']
+const text =
+    `
+    CREATE TABLE test(
+        test_id SERIAL UNIQUE,
+        name VARCHAR(255)
+    );
+    `
+
+
 pgConnect = async () => {
     await client.connect()
-    const res = await client.query('SELECT $1::text as message', ['Hello world!'])
-    console.log(res.rows[0].message) // Hello world!
-    // const res2 = await client.query('SELECT * FROM film LIMIT 1;')
-    // console.log(res2)
+    try {
+        const res = await client.query(text)
+        console.log(res)
+    } catch (err) {
+        console.log(err.stack)
+    }
+
+    // const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+    // console.log(res.rows[0].message) // Hello world!
+    // // const res2 = await client.query('SELECT * FROM film LIMIT 1;')
+    // // console.log(res2)
     await client.end()
 }
 
