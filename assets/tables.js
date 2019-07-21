472035b91f0d753f778e3f5e1194cef4ae7f765f
Logan Wohlers <lawohlers@gmail.com>
2019-07-20 18:03:07 -0700

    seeding teams into pg

--- assets/tables.js
+++ assets/tables.js
@@ -8,7 +8,7 @@ CREATE TABLE teams
     conference VARCHAR(255) NOT NULL,
     division VARCHAR(255) NOT NULL,
     team_code VARCHAR(255) UNIQUE NOT NULL
-);`
+)`
 
 const seasons_table = `
 CREATE TABLE seasons
@@ -16,6 +16,6 @@ CREATE TABLE seasons
     season_id SERIAL UNIQUE PRIMARY KEY,
     year INTEGER UNIQUE NOT NULL,
     description VARCHAR(255) UNIQUE NOT NULL,
-);`
+)`
 
 module.exports = { teams_table, seasons_table }
--- src/db/pg.js
+++ src/db/pg.js
@@ -1,5 +1,6 @@
 const { Client } = require('pg')
-const { teams_table } = require('../../assets/tables')
+const { teams_table, seasons_table } = require('../../assets/tables')
+const teams = require('../../assets/teams')
 
 const connectionString = 'postgresql://postgres:password@localhost:5432/nbanode'
 
@@ -7,19 +8,34 @@ const client = new Client({
     connectionString
 })
 
-
-
-
-
 pgConnect = async () => {
     await client.connect()
     try {
-        const res = await client.query(teams_table)
-        console.log(res)
+        const tt = await client.query(teams_table)
+        await seedTeams()
+        console.log('tables created!')
     } catch (err) {
         console.log(err.stack)
     }
     await client.end()
+}
+
+
+seedTeams = async () => {
+    const text = `
+    INSERT INTO teams (name, full_name, city, conference, division, team_code)
+    VALUES ($1, $2, $3, $4, $5, $6)`
+    try {
+        for (let i = 0; i < teams.length; i++) {
+            const { name, fullName, city, conference, division, teamCode } = teams[i]
+            let values = [name, fullName, city, conference, division, teamCode]
+
+            const res = await client.query(text, values)
+            console.log(res)
+        }
+    } catch (e) {
+        return console.log(e)
+    }
 }
 
 pgConnect()
